970ff2db87920d8e18b0ed6d95182fe7f917c0a7
Billy Burrows <hello@iamjawa.co>
2018-05-09 14:20:44 +0100

    Updated Key Data to Variables
    Initially key data such as login information were hardcoded throughout the script, these have all been updated to variables instead.

--- script.php
+++ script.php
@@ -5,7 +5,10 @@ $serverlocation = ""; // Network Location of the MySQL installation.
 $username = ""; // Username used for logging into the MySQL server.
 $password = ""; // Password used for logging into the MySQL server.
 $databasename = ""; // Name of the database that the email table is stored in.
-
+$tableName = ""; // mySQL Table where the emails are stored
+$emailColumn = ""; // mySQL Column Name the Email addresses are stored in
+$idColumn = ""; // mySQL Column Name the ID's of the Email addresses are stored in
+$rootSaveLocation = ""; // Save location for images, please add a trailing slash
 
 $connector = new mysqli( $serverlocation, $username, $password, $databasename ); // Sets the variable $connector as the credentials needed to login to the database.
 
@@ -15,7 +18,7 @@ if ( $connector->connect_error ) {
 }
 
 
-$sql = "SELECT <ID Column>, <Email Column> FROM <Table containing Email Addresses>"; // Attempts to select the columns ID and address from the Table addresses in the database.
+$sql = "SELECT " . $idColumn . " , " . $emailColumn . " FROM " . $tableName; // Attempts to select the columns ID and address from the Table addresses in the database.
 $result = $connector->query( $sql ); // Sets the variable $result as a query to the database.
 
 
@@ -24,26 +27,26 @@ if ( $result->num_rows > 0 ) { // Checks if Rows exist within the table
 
     while ( $row = $result->fetch_assoc() ) { // Fetches Rows from table
 
-        mkdir ('/root/to/save/location', 777, true); // Creates the root directory for the image directories to be stored in
-        mkdir( '/root/to/save/location/' . $row["<Email Address Column>"] . '/' , 777, true ); // Creates the necessary directories for the images to be saved in.
+        mkdir($rootSaveLocation, 777, true); // Creates the root directory for the image directories to be stored in
+        mkdir( $rootSaveLocation . $row[$emailColumn] . '/' , 777, true ); // Creates the necessary directories for the images to be saved in.
         $imagesizes = array( '40', '80', '200' ); // Array defining the image sizes to be saved in px (e.g 190 in the array represents an 190px * 190px image)
-        $htmlimglink = '<img src="http://secure.gravatar.com/avatar/' .md5( $row["<Email Column>"] ). '&d=404/">'; // Builds the HTML URL of the Gravatars for the images to be displayed on the final page from once they are saved (Standard 80px).
-        $directory = '/root/to/save/location' . '/' . $row["<Email Address Column>"] . '/'; // Sets the directories for the images to be saved in to the directories created earlier.
+        $htmlimglink = '<img src="http://secure.gravatar.com/avatar/' .md5( $row[$emailColumn] ). '&d=404/">'; // Builds the HTML URL of the Gravatars for the images to be displayed on the final page from once they are saved (Standard 80px).
+        $directory = $rootSaveLocation . '/' . $row[$emailColumn] . '/'; // Sets the directories for the images to be saved in to the directories created earlier.
 
 
         foreach ( $imagesizes as $urlsizes ) { // Begins cycling the array of image sizes
-            $genericurl = 'http://secure.gravatar.com/avatar/' .md5( $row["<Email Column>"] ). '?s=' . $urlsizes . '&d=404/'; // Builds the URL to get the image for each address from and returns a 404 error if no Gravatar is found.
+            $genericurl = 'http://secure.gravatar.com/avatar/' .md5( $row[$emailColumn] ). '?s=' . $urlsizes . '&d=404/'; // Builds the URL to get the image for each address from and returns a 404 error if no Gravatar is found.
             $content = file_get_contents( $genericurl ); // Actually fetches the images from the url built previously
             file_put_contents( $directory . 'profile_photo-' . $urlsizes . '.jpg', $content ); // Saves the images in their respective directories.
-            shell_exec('find /root/to/save/location/ -empty -type f -delete'); // Finds Empty Files in the root image directory and subdirectories and Removes them (If the email doesn't have a Gravatar set)
-            shell_exec('find /root/to/save/location/ -empty -type d -delete'); // Finds Empty Directories in the root image directory and Removes them (For directories that contain(ed) empty files when emails don't have a Gravatar set)
+            shell_exec('find ' . $rootSaveLocation . ' -empty -type f -delete'); // Finds Empty Files in the root image directory and subdirectories and Removes them (If the email doesn't have a Gravatar set)
+            shell_exec('find ' . $rootSaveLocation . ' -empty -type d -delete'); // Finds Empty Directories in the root image directory and Removes them (For directories that contain(ed) empty files when emails don't have a Gravatar set)
         }
 
-        shell_exec('chmod -R 0755 ~/root/to/save/location'); // Sets the permissions of the root folder and all of its subdirectories to 755 for added security (Useful for Web Servers for example)
+        shell_exec('chmod -R 0755 ' . $rootSaveLocation); // Sets the permissions of the root folder and all of its subdirectories to 755 for added security (Useful for Web Servers for example)
 
 
-        echo "ID: " . $row["<ID Column>"]. " - Email Hash: " . md5( $row["<Email Column>"] ). " " . "<br>"; // Displays the ID and Hash of each individual record once the images have been saved.
-        echo "Email: " . $row["<Email Column>"]. " " . "<br><br>"; // Displays the actual Email address of each indiviidual record once the images have been saved.
+        echo "ID: " . $row[$idColumn]. " - Email Hash: " . md5( $row[$emailColumn] ). " " . "<br>"; // Displays the ID and Hash of each individual record once the images have been saved.
+        echo "Email: " . $row[$emailColumn]. " " . "<br><br>"; // Displays the actual Email address of each indiviidual record once the images have been saved.
         echo $htmlimglink; // Displays the Gravatars to the page from the HTML URL built in the variable $htmlimglink.
         echo "<br /><br />"; // General Styling
         echo "<hr />"; // General Styling
